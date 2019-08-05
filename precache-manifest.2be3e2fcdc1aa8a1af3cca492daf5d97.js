32c12ae594eccb1e2e5d0aa147566c8f87762a17
Cristiana Man <cma@templafy.com>
2019-08-05 22:51:01 +0200

    Updates

---
+++ precache-manifest.2be3e2fcdc1aa8a1af3cca492daf5d97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "307cb19b7528e4c08773",
+    "url": "/ignite/static/css/main.66001462.chunk.css"
+  },
+  {
+    "revision": "307cb19b7528e4c08773",
+    "url": "/ignite/static/js/main.307cb19b.chunk.js"
+  },
+  {
+    "revision": "8176aada5eb66e435d5f",
+    "url": "/ignite/static/js/runtime~main.8176aada.js"
+  },
+  {
+    "revision": "b901e4fa15a0e16250dc",
+    "url": "/ignite/static/js/2.b901e4fa.chunk.js"
+  },
+  {
+    "revision": "8ad1e4193bd68df78cd0c0b38cd5c111",
+    "url": "/ignite/static/media/bg.8ad1e419.png"
+  },
+  {
+    "revision": "15e4b431f96c17234a305303bdba5faf",
+    "url": "/ignite/index.html"
+  }
+];
