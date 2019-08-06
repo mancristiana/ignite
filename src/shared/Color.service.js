7bd3aefce33217be631b4acb086f363c35857ccb
Cristiana Man <cma@templafy.com>
2019-08-06 17:38:12 +0200

    Update conference data, colors

--- src/shared/Color.service.js
+++ src/shared/Color.service.js
@@ -1,11 +1,17 @@
 export function generateColor(text) {
   switch (text) {
-    case "Productivity & Library + Library Admin":
+    case "Productivity & Library":
+      return "#009688";
+    case "Library Admin":
       return "#8bc34a";
-    case "Dynamics + Integrations":
+    case "Dynamics":
       return "#9c27b0";
-    case "Platform + Email Signature":
+    case "Integrations":
+      return "#673ab7";
+    case "Platform":
       return "#3f51b5";
+    case "Email Signature":
+      return "#2196f3";
     case "Social":
       return "#ffc107";
     default: return "#000"
